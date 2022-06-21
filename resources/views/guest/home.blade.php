0eec3437d733e2e4368aa0fb5408d80f8eb682f7
Fabrizio Zivolo <fabrizio.zivolo@gmail.com>
2022-06-21 17:10:04 +0200

    creato test layout navbar

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,33 +6,48 @@
 
         <title>Boolpress</title>
 
-        <!-- Fonts -->
         <link href="{{asset('css/front.css') }}" rel="stylesheet">
-
-        
+      
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <body class="bg-dark">
+        <nav class="navbar navbar-expand-lg bg-secondary">
             @if (Route::has('login'))
-                <div class="top-right links">
+            <a class="navbar-brand text-danger" href="{{ url('/admin') }}">Boolpress</a>
+
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
                     @auth
-                        <a href="{{ url('/admin') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+                  <a class="nav-link text-light" href="{{ url('/admin') }}">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-light" href="#">Link</a>
+                </li>
+              </ul>
+              @else
+              <!-- <form class="form-inline my-2 my-lg-0"> 
+                <ul class="navbar-nav mr-auto"> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                    @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Boolpress
-                </div>
+
+                </ul>
+            </form>
+
+               
+            @endauth
 
             </div>
-        </div>
+            @endif
+          </nav>
+
     </body>
 </html>
